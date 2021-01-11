4797aab9e47ca8ee065f255f11c08897b9ddc2fa
Wanlin-Lu <luwanlin@126.com>
2021-01-11 20:44:48 +0800

    在事件循环中DOM渲染的时机

--- i.js
+++ i.js
@@ -4,30 +4,16 @@ const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-function multi(num) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(num * num)
-    }, 1000);
-  }) 
-}
+const $p1 = $('<p>一段文字</p>')
+const $p2 = $('<p>一段文字</p>')
+const $p3 = $('<p>一段文字</p>')
 
-function test1() {
-  const arr = [1, 2, 3];
-  arr.forEach(async x => {
-    const res = await multi(x)
-    log(res)
-  })
-}
+$('#root').append($p1).append($p2).append($p3)
 
-test1()
+log('length', $('#root').children().length)
 
-async function test2() {
-  const arr = [1, 2, 3];
-  for (let x of arr) {
-    const res = await multi(x)
-    log(res)
-  }
-}
+alert('本次call stack结束，DOM已经更新，但是尚未触发渲染')
 
-test2()
+setTimeout(() => {
+ alert('定时器回调函数执行，意味着第二次call stack，就能看到dom了。') 
+});
