5733404799544c0dd45461281d7d924d1a457226
Wanlin-Lu <luwanlin@126.com>
2021-01-11 21:38:13 +0800

    数字千分位分隔符 ，

--- i.js
+++ i.js
@@ -4,16 +4,14 @@ const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-const $p1 = $('<p>一段文字</p>')
-const $p2 = $('<p>一段文字</p>')
-const $p3 = $('<p>一段文字</p>')
+let num = 123412341234214.123
 
-$('#root').append($p1).append($p2).append($p3)
+function commafy(num) {
+  return num && num
+    .toString()
+    .replace(/(\d)(?=(\d{3})+\.)/g, function ($1, $2) {
+    return $2 + ','
+  })
+}
 
-log('length', $('#root').children().length)
-
-alert('本次call stack结束，DOM已经更新，但是尚未触发渲染')
-
-setTimeout(() => {
- alert('定时器回调函数执行，意味着第二次call stack，就能看到dom了。') 
-});
+log(commafy(num))
