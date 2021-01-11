c77d0f1b3d7b2dcabf0c289d1a4f2718727911a4
Wanlin-Lu <luwanlin@126.com>
2021-01-11 14:11:44 +0800

    jQuery self design

--- i.js
+++ i.js
@@ -2,44 +2,50 @@
 const { log } = console
 
 /* play-ground */
-class People {
-  constructor(name) {
-    this.name = name;
+class jQuery {
+  constructor(selector) {
+    const result = document.querySelectorAll(selector)
+    const length = result.length 
+    for (let i = 0; i < length; i++) {
+      this[i] = result[i]
+    }
+    this.length = length
+    this.selector = selector
   }
-  walk() {
-    log(this.name,' walking!')
+
+  get(index) {
+    return this[index]
+  }
+
+  each(fn) {
+    for (let i = 0; i < this.length; i++) {
+      const elem = this[i]
+      fn(elem)
+    }
+  }
+
+  on(type, fn) {
+    return this.each(elem => {
+      elem.addEventListener(type, fn, false)
+    })
   }
 }
 
-class Student extends People {
-  constructor(name, number) {
-    super(name)
-    this.number = number
+jQuery.prototype.dialog = function (info) {
+  log(info)
+}
+
+class myjQuery extends jQuery {
+  constructor(selector) {
+    super(selector)
   }
-  study() {
-    log(`${this.name} number is ${this.number}, is study!`)
+
+  style(data) {
+    log(data)
   }
 }
 
-const zhangsan = new People('zhangsan')
-log(zhangsan.walk())
-
-const xiaoming = new Student('xiaoming', 1000)
-log(xiaoming)
-log(xiaoming.study())
-log(xiaoming.walk())
-
-log(xiaoming.__proto__ === Student.prototype)
-log(zhangsan.__proto__ === People.prototype)
-log(Student.__proto__ === People)
-log(Student.prototype.__proto__ === People.prototype)
-
-xiaoming.__proto__.__proto__.sleep = function () {
-  console.log('sleeping!')
-}
-log(zhangsan.sleep())
-log(xiaoming.sleep())
-
-log(xiaoming instanceof Student)
-log(xiaoming instanceof People)
-log(xiaoming instanceof Object)
+const $p = new jQuery('p')
+log($p.get(1))
+$p.each(elem => console.log(elem.nodeName))
+$p.on('click', () => alert('click'))
