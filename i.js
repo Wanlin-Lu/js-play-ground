d416e268802f7f84dd0456e9f8607d916caf169b
Wanlin-Lu <luwanlin@126.com>
2021-01-12 11:05:10 +0800

    Generator函数 flat数组 | for-of

--- i.js
+++ i.js
@@ -4,21 +4,22 @@ const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-const queuedObservers = new Set()
 
-const observe = fn => queuedObservers.add(fn)
-const observable = obj => new Proxy(obj, { set });
-
-function set(target, key, value, receiver) {
-  const result = Reflect.set(target, key, value, receiver)
-  queuedObservers.forEach(observer => observer())
-  return result
+const flat = function* (a) {
+  let len = a.length
+  for (let i = 0; i < len; i++) {
+    let item = a[i]
+    if (item instanceof Array) {
+      yield* flat(item)
+    } else {
+      yield item
+    }
+  }
 }
 
-let obj = {}
-let print = function () { log('printed') }
+const arr = [1, [2, [3, [4, 5], [6, 7]]]]
+log([...flat(arr)])
 
-observe(print)
-let obj1 = observable(obj)
-
-obj1.name = 'asang'
+for (let v of flat(arr)) {
+  log(v)
+}
