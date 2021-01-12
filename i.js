51bb5a2ccc858119bb60cea64111ced8ac0b920b
Wanlin-Lu <luwanlin@126.com>
2021-01-12 11:17:48 +0800

    async/await | Promise | setTimeout 实现休眠语法

--- i.js
+++ i.js
@@ -4,22 +4,17 @@ const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
+function sleep(interval) {
+  return new Promise(resolve => {
+    setTimeout(resolve,interval)
+  })
+}
 
-const flat = function* (a) {
-  let len = a.length
-  for (let i = 0; i < len; i++) {
-    let item = a[i]
-    if (item instanceof Array) {
-      yield* flat(item)
-    } else {
-      yield item
-    }
+async function one2five() {
+  for (let i = 1; i < 6; i++) {
+    console.log(i)
+    await sleep(1000)
   }
 }
 
-const arr = [1, [2, [3, [4, 5], [6, 7]]]]
-log([...flat(arr)])
-
-for (let v of flat(arr)) {
-  log(v)
-}
+one2five()
