0da1aa2f247dea3a765b3276c07df0b0c2eb37ab
Wanlin-Lu <luwanlin@126.com>
2021-01-12 11:31:01 +0800

    async/await | map | for-of 实现并发，顺序输出

--- i.js
+++ i.js
@@ -4,17 +4,31 @@ const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-function sleep(interval) {
-  return new Promise(resolve => {
-    setTimeout(resolve,interval)
-  })
-}
+let todoUrls = [
+  'https://jsonplaceholder.typicode.com/todos/1',
+  'https://jsonplaceholder.typicode.com/todos/2',
+  'https://jsonplaceholder.typicode.com/todos/3',
+  'https://jsonplaceholder.typicode.com/todos/4',
+]
 
-async function one2five() {
-  for (let i = 1; i < 6; i++) {
-    console.log(i)
-    await sleep(1000)
+async function loadOrder(urls) {
+  for (let url of urls) {
+    const response = await fetch(url)
+    log(await response.json())
   }
 }
 
-one2five()
+// loadOrder(todoUrls)
+
+async function loadOrderBF(urls) {
+  const jsonPromises = urls.map(async url => {
+    const response = await fetch(url)
+    return response.json()
+  })
+
+  for (let jsonPromise of jsonPromises) {
+    log(await jsonPromise)
+  }
+}
+
+loadOrderBF(todoUrls)
