1f11bfa4e03550a4e7aa93ddc4a39398c084fe36
Wanlin-Lu <luwanlin@126.com>
2021-01-12 12:37:19 +0800

    通用的bindEvent : 事件代理和普通绑定

--- i.js
+++ i.js
@@ -1,34 +1,38 @@
 /* util */
 const { log } = console
 const todourl = 'https://jsonplaceholder.typicode.com/todos/2'
-const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
-
-/* play-ground */
 let todoUrls = [
   'https://jsonplaceholder.typicode.com/todos/1',
   'https://jsonplaceholder.typicode.com/todos/2',
   'https://jsonplaceholder.typicode.com/todos/3',
   'https://jsonplaceholder.typicode.com/todos/4',
 ]
+const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
-async function loadOrder(urls) {
-  for (let url of urls) {
-    const response = await fetch(url)
-    log(await response.json())
+/* play-ground */
+
+function bindEvent(elem, type, selector, fn) {
+  if (fn == null) {
+    fn = selector
+    selector = null
   }
-}
 
-// loadOrder(todoUrls)
-
-async function loadOrderBF(urls) {
-  const jsonPromises = urls.map(async url => {
-    const response = await fetch(url)
-    return response.json()
+  elem.addEventListener(type, event => {
+    const target = event.target
+    if (selector) {
+      if (target.matches(selector)) {
+        fn.call(target, event)
+      }
+    } else {
+      fn.call(target, event)
+    }
   })
-
-  for (let jsonPromise of jsonPromises) {
-    log(await jsonPromise)
-  }
 }
 
-loadOrderBF(todoUrls)
+const pb = document.getElementById('pbox')
+const cb = document.getElementById('cbox')
+const items = document.getElementsByClassName('list')
+
+bindEvent(items[0], 'click', e => log('li clicked!'))
+bindEvent(pb, 'click', 'li', (e) => log('li clicked delegate'))
+
