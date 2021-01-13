f87492b297dc53ca16578c872c962392be79bd52
Wanlin-Lu <luwanlin@126.com>
2021-01-13 21:21:31 +0800

    map返回新数组，也可以将遍历操作结果外存

--- i.js
+++ i.js
@@ -10,20 +10,24 @@ let todoUrls = [
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-/* let arr = [1, 2, 3]
-log(arr)
-arr.forEach(i => i + 2)
-log(arr)
+// let arr = [1, 2, 3]
+// log(arr)
+// arr.map(i => i + 2)
+// let arr1 = arr.map((i) => i + 2)
+// log(arr)
+// log('arr1',arr1)
 
-let arr1 = []
-arr.forEach(i => arr1.push(i + 2))
-log(arr1) */
+// let arr2 = []
+// arr.map(i => arr2.push(i + 2))
+// log(arr2)
 
 let arr = [{a:2,b:3}, {a:2,b:3}, {a:2,b:3}]
 log(arr)
-arr.forEach((i) => i.a + 2)
+arr.map((i) => i.a + 2)
+let arr3 = arr.map((i) => ({ ...i, a: i.a + 2 }))
 log(arr)
+log('arr3',arr3)
 
-let arr1 = []
-arr.forEach((i) => arr1.push({...i,a: i.a + 2}))
-log(arr1)
+let arr4 = []
+arr.map((i) => arr4.push({...i,a: i.a + 2}))
+log(arr4)
