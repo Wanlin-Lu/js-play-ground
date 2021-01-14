6d59913a7a3577140a406fd73ca297f1fb98a769
Wanlin-Lu <luwanlin@126.com>
2021-01-14 11:01:10 +0800

    adjust files

--- i.js
+++ i.js
@@ -9,8 +9,64 @@ let todoUrls = [
 ]
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
-/* play-ground */
-function myInstanceof(left, right) {
+/*
+ *js语言基础
+ */
+
+/* 操作符和表达式 */
+ 
+/* 语句 */
+
+/* 
+ * js 数据类型
+ */
+/* 数值 */
+/* 字符串 */
+/* 对象 */
+/* 数组 */
+/* 函数 */
+/* Date日期 */
+/* 正则表达式 */
+/* JSON */
+/* Symbol */
+/* Set & WeakSet */
+/* Map & WeakMap */
+
+/* 
+ * js 进阶
+ */
+/* 类型进阶 */
+/* deepClone */
+/* function deepClone(obj) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj
+  }
+
+  let result
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key])
+    }
+  }
+
+  return result
+}
+
+let arr = [1, 2, 3]
+let arr1 = arr
+let arr2 = deepClone(arr)
+arr1[0] = 1000
+arr2[1] = 2000
+log(arr) //[1000,2,3] */
+
+/* myInstanceof */
+/* function myInstanceof(left, right) {
   if (typeof left !== 'object' || left === null) return false
   
   let prototype = right.prototype
@@ -22,5 +78,94 @@ function myInstanceof(left, right) {
   }
 }
 
-log(myInstanceof([1,2],Array))
+log(myInstanceof([1,2],Array)) */
 
+/* 函数进阶 */
+/* 变量作用域进阶 */
+/* this -> 箭头函数 */
+/* const zhangsan = {
+  name: 'zhangsan',
+  sayHi() {
+    //this 即当前对象
+    console.log(this)
+  },
+  wait() {
+    setTimeout(() => {
+      //this 为当前对象
+      console.log(this)
+    })
+  },
+  see() {
+    setTimeout(function () {
+      //this timeout
+      console.log(this)
+    })
+  },
+}
+zhangsan.sayHi()
+zhangsan.wait()
+zhangsan.see() */
+
+/* 原型进阶 */
+/* 手写jQuery */
+/* class jQuery {
+  constructor(selector) {
+    const result = document.querySelectorAll(selector)
+    const length = result.length
+    for (let i = 0; i < length; i++) {
+      this[i] = result[i]
+    }
+
+    this.length = length
+    this.selector = selector
+  }
+
+  get(index) {
+    return this[index]
+  }
+
+  each(fn) {
+    for (let i = 0; i < this.length; i++) {
+     const elem = this[i]
+      fn(elem)
+    }
+  }
+
+  on(type, fn) {
+    return this.each((elem) => {
+      elem.addEventListener(type, fn, false)
+    })
+  }
+}
+
+jQuery.prototype.dialog = function (info) {
+  alert(info)
+}
+
+class myJQuery extends jQuery {
+  constructor(selector) {
+    super(selector)
+  }
+
+  style(data) {
+    log(data)
+  }
+}
+
+const $p = new jQuery('p')
+log($p.get(1))
+
+$p.each((elem) => log(elem.nodeName))
+$p.on('click', () => alert('click'))  */
+
+/* 变量的解构赋值 */
+/* Iterator(遍历器)和for-of循环 */
+/* Proxy & Reflect */
+
+/* Promise */
+
+/* Generator */
+
+/* async */
+
+/* play-ground */
