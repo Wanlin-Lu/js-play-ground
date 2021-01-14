90686b1e404d9484343622730601224637c49661
Wanlin-Lu <luwanlin@126.com>
2021-01-14 10:11:04 +0800

    类型进阶：deepClone

--- 03jj.js
+++ 03jj.js
@@ -1,4 +1,33 @@
 /* 类型进阶 */
+/* deepClone */
+/* function deepClone(obj) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj
+  }
+
+  let result
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key])
+    }
+  }
+
+  return result
+}
+
+let arr = [1, 2, 3]
+let arr1 = arr
+let arr2 = deepClone(arr)
+arr1[0] = 1000
+arr2[1] = 2000
+log(arr) //[1000,2,3] */
+
 /* 函数进阶 */
 /* 变量作用域进阶 */
 /* 原型进阶 */
--- i.js
+++ i.js
@@ -10,27 +10,5 @@ let todoUrls = [
 const imgurl = `https://adom-wanlinlu-blog.oss-cn-hangzhou.aliyuncs.com/avatar.png`
 
 /* play-ground */
-var bip = document.getElementById('bibao')
-var list = bip.getElementsByTagName('li')
 
-/* for (var i = 0; i < list.length; i++) {
-  list[i].onclick = (function (i) {
 
-    return function () {
-      alert(i + 1)
-    }
-
-  })(i)
-} */
-
-for (var i = 0; i < list.length; i++) {
-  list[i].onclick = function () {
-    alert(i+1)
-  }
-}
-
-/* for (let i = 0; i < list.length; i++) {
-  list[i].onclick = function () {
-    alert(i+1)
-  }
-} */
